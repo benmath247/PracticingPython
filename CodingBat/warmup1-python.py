810174de99fea4c4d58aeea5e68977d52fe0b946
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-23 22:21:47 -0500

    pos_neg

--- CodingBat/warmup1-python.py
+++ CodingBat/warmup1-python.py
@@ -98,3 +98,18 @@ near_hundred(89) → False
 """
 def near_hundred(n):
     return (n>=90 and n<=110) or (n>=190 and n<=210)
+
+"""
+Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+
+
+pos_neg(1, -1, False) → True
+pos_neg(-1, 1, False) → True
+pos_neg(-4, -5, True) → True
+"""
+def pos_neg(a, b, negative):
+  if negative:
+    return a<0 and b<0
+  else:
+    return ((a<0 and b>0) or (b<0 and a>0))
+   

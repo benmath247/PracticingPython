49a1795cb91f03cbeda476ff936fee47c2c76b48
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-23 22:12:10 -0500

    makes10

--- CodingBat/warmup1-python.py
+++ CodingBat/warmup1-python.py
@@ -12,10 +12,10 @@ sleep_in(False, True) → True'
 """
 
 def sleep_in(weekday, vacation):
-  if not weekday or vacation:
-    return True
-  else:
-    return False
+    if not weekday or vacation:
+        return True
+    else:
+        return False
 
 """
 We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
@@ -26,12 +26,12 @@ monkey_trouble(False, False) → True
 monkey_trouble(True, False) → False
 """
 def monkey_trouble(a_smile, b_smile):
-  if a_smile and b_smile:
-    return True
-  if not a_smile and not b_smile:
-    return True
-  else:
-    return False
+    if a_smile and b_smile:
+        return True
+    if not a_smile and not b_smile:
+        return True
+    else:
+        return False
 
 """
 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
@@ -43,9 +43,9 @@ sum_double(2, 2) → 8
 """
 
 def sum_double(a, b):
-  if a==b:
-    return (a+b)*2
-  return a+b
+    if a==b:
+        return (a+b)*2
+    return a+b
 
 """
 Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
@@ -56,10 +56,10 @@ diff21(10) → 11
 diff21(21) → 0
 """
 def diff21(n):
-  if n<=21:
-    return 21-n
-  else:
-    return (n-21)*2
+    if n<=21:
+        return 21-n
+    else:
+        return (n-21)*2
   
 
 """
@@ -73,5 +73,17 @@ parrot_trouble(False, 6) → False
 def parrot_trouble(talking, hour):
   if hour<7 or hour>20:
     if talking:
-      return True
-  return False
+        return True
+    return False
+
+"""
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+
+makes10(9, 10) → True
+makes10(9, 9) → False
+makes10(1, 9) → True
+"""
+def makes10(a, b):
+#  return (a==10 or b==10) or ((a+b)==10)
+    return (a==10 or b==10 or (a+b)==10)

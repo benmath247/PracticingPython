3a46ccf25f1193bb3d5f2f1c73f259b176137e36
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-29 13:01:59 -0500

    warmup1 unittests and format fixes

--- CodingBat/warmup1_python.py
+++ CodingBat/warmup1_python.py
@@ -1,3 +1,4 @@
+import unittest
 """
 sleep-in
 The parameter weekday is True if it is a weekday, 
@@ -17,8 +18,13 @@ def sleep_in(weekday, vacation):
     else:
         return False
 
+assert sleep_in(False, False) == True
+assert sleep_in(True, False) == False
+assert sleep_in(False, True) == True
+
 """
-We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. 
+We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
 
 monkey_trouble(True, True) → True
@@ -32,6 +38,10 @@ def monkey_trouble(a_smile, b_smile):
         return True
     else:
         return False
+
+assert monkey_trouble(True, True) == True
+assert monkey_trouble(False, False) == True
+assert monkey_trouble(True, False) == False
 
 """
 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
@@ -47,6 +57,10 @@ def sum_double(a, b):
         return (a+b)*2
     return a+b
 
+assert sum_double(1, 2) == 3
+assert sum_double(3, 2) == 5
+assert sum_double(2, 2) == 8
+
 """
 Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 
@@ -60,10 +74,15 @@ def diff21(n):
         return 21-n
     else:
         return (n-21)*2
+
+assert diff21(19) == 2
+assert diff21(10) == 11
+assert diff21(21) == 0
   
 
 """
-We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
 
 
 parrot_trouble(True, 6) → True
@@ -75,6 +94,10 @@ def parrot_trouble(talking, hour):
     if talking:
         return True
     return False
+
+assert parrot_trouble(True, 6) == True
+assert parrot_trouble(True, 7) == False
+assert parrot_trouble(False, 6) == False
 
 """
 Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
@@ -88,6 +111,10 @@ def makes10(a, b):
 #  return (a==10 or b==10) or ((a+b)==10)
     return (a==10 or b==10 or (a+b)==10)
 
+assert makes10(9, 10) == True
+assert makes10(9, 9) == False
+assert makes10(1, 9) == True
+
 """
 Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
 
@@ -99,8 +126,13 @@ near_hundred(89) → False
 def near_hundred(n):
     return (n>=90 and n<=110) or (n>=190 and n<=210)
 
+assert near_hundred(93) == True
+assert near_hundred(90) == True
+assert near_hundred(89) == False
+
 """
-Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, 
+then return True only if both are negative.
 
 
 pos_neg(1, -1, False) → True
@@ -112,9 +144,14 @@ def pos_neg(a, b, negative):
         return a<0 and b<0
     else:
         return ((a<0 and b>0) or (b<0 and a>0))
+
+assert pos_neg(1, -1, False) == True
+assert pos_neg(-1, 1, False) == True
+assert pos_neg(-4, -5, True) == True
    
 """
-Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+Given a string, return a new string where "not " has been added to the front. 
+However, if the string already begins with "not", return the string unchanged.
 
 
 not_string('candy') → 'not candy'
@@ -127,8 +164,13 @@ def not_string(str):
     else:
         return "not "+str
 
+assert not_string('candy') == 'not candy'
+assert not_string('x') == 'not x'
+assert not_string('not bad') == 'not bad'
+
 """
-Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
 
 
 missing_char('kitten', 1) → 'ktten'
@@ -137,6 +179,10 @@ missing_char('kitten', 4) → 'kittn'
 """
 def missing_char(str, n):
     return str[0:n]+str[n+1:len(str)]
+
+assert missing_char('kitten', 1) == 'ktten'
+assert missing_char('kitten', 0) == 'itten'
+assert missing_char('kitten', 4) == 'kittn'
 
 """
 Given a string, return a new string where the first and last chars have been exchanged.
@@ -152,9 +198,15 @@ def front_back(str):
     else:
         return str[len(str)-1]+str[1:len(str)-1]+str[0]
 
+assert front_back('code') == 'eodc'
+assert front_back('a') == 'a'
+assert front_back('ab') == 'ba'
+
 """
 
-Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+Given a string, we'll say that the front is the first 3 chars of the string. 
+If the string length is less than 3, the front is whatever is there. 
+Return a new string which is 3 copies of the front.
 
 
 front3('Java') → 'JavJavJav'
@@ -162,8 +214,14 @@ front3('Chocolate') → 'ChoChoCho'
 front3('abc') → 'abcabcabc'
 """
 def front3(str):
-  if len(str) < 3:
-    front = str
-  else:
-    front = str[0:3]
-  return front+front+front
+    if len(str) < 3:
+        front = str
+    else:
+        front = str[0:3]
+    return front+front+front
+
+assert front3('Java') == 'JavJavJav'
+assert front3('Chocolate') == 'ChoChoCho'
+assert front3('abc') == 'abcabcabc'
+
+

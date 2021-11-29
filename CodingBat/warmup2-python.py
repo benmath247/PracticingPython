c1da9091846b9046c574b67a96144506e6e8e361
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-29 11:57:50 -0500

    every other letter

--- CodingBat/warmup2-python.py
+++ CodingBat/warmup2-python.py
@@ -24,3 +24,18 @@ def front_times(str, n):
     return str[0:3] * n
   
 
+"""
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello'
+"""
+
+def string_bits(str):
+    result = ""
+    for i in range(len(str)):
+        if i % 2 == 0:
+        result += str[i]
+    return result

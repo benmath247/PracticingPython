05be440c8c38508585ba79ba128d8f03b1c63cec
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-29 12:00:56 -0500

    string explosion

--- CodingBat/warmup2-python.py
+++ CodingBat/warmup2-python.py
@@ -39,3 +39,17 @@ def string_bits(str):
         if i % 2 == 0:
         result += str[i]
     return result
+
+"""
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'
+"""
+def string_splosion(str):
+    result = ""
+    for i in range(len(str)+1):
+        result += str[0:i]
+    return result

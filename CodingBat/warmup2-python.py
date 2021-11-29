deeafa0ae48892695cf876d1fed6784388012ce5
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-29 12:09:57 -0500

    array_count9

--- CodingBat/warmup2-python.py
+++ CodingBat/warmup2-python.py
@@ -75,3 +75,20 @@ def last2(str):
             result += 1
   
     return result
+
+"""
+Given an array of ints, return the number of 9's in the array.
+
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3
+"""
+
+def array_count9(nums):
+    result = 0
+    for i in nums:
+        if i == 9:
+            result += 1
+    return result
+

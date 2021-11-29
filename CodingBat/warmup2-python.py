d9d223388493c4dc509de3732c3ad9b746e8d818
Ben Math <benjamin.aaron.math@gmail.com>
2021-11-29 12:18:54 -0500

    array_front9

--- CodingBat/warmup2-python.py
+++ CodingBat/warmup2-python.py
@@ -92,3 +92,23 @@ def array_count9(nums):
             result += 1
     return result
 
+
+"""
+Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+array_front9([1, 2, 9, 3, 4]) → True
+array_front9([1, 2, 3, 4, 9]) → False
+array_front9([1, 2, 3, 4, 5]) → False
+"""
+def array_front9(nums):
+    if len(nums) <= 4:
+        num = len(nums)
+    else:
+        num = 4
+  
+    for i in range(num):
+        if nums[i] == 9:
+            return True
+    return False
+

4deaa879f0d1cb48759f4b9a3c8874e7ba6495de
Ben Math <benjamin.aaron.math@gmail.com>
2021-12-09 15:57:11 -0500

    logic2 added

--- CodingBat/list1.py
+++ CodingBat/list1.py
@@ -65,3 +65,114 @@ def sum3(nums):
     for i in nums:
         result += i
     return result
+
+
+"""Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7]"""
+
+
+def rotate_left3(nums):
+    return [nums[1], nums[2], nums[0]]
+
+
+"""
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+"""
+
+
+def reverse3(nums):
+    return [nums[2], nums[1], nums[0]]
+
+
+"""
+Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+
+max_end3([1, 2, 3]) → [3, 3, 3]
+max_end3([11, 5, 9]) → [11, 11, 11]
+max_end3([2, 11, 3]) → [3, 3, 3]
+"""
+
+
+def max_end3(nums):
+    largest = 0
+    if nums[0] > nums[2]:
+        largest = nums[0]
+    else:
+        largest = nums[2]
+    return [largest, largest, largest]
+
+
+"""
+Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+
+sum2([1, 2, 3]) → 3
+sum2([1, 1]) → 2
+sum2([1, 1, 1, 1]) → 2
+"""
+
+
+def sum2(nums):
+    if len(nums) >= 2:
+        return nums[0] + nums[1]
+    elif len(nums) == 1:
+        return nums[0]
+    return 0
+
+
+"""
+
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+
+middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+"""
+
+
+def middle_way(a, b):
+    return [a[1], b[1]]
+
+
+"""
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+"""
+
+
+def make_ends(nums):
+    return [nums[0], nums[len(nums) - 1]]
+
+
+"""
+
+Given an int array length 2, return True if it contains a 2 or a 3.
+
+
+has23([2, 5]) → True
+has23([4, 3]) → True
+has23([4, 5]) → False
+"""
+
+
+def has23(nums):
+    if nums[0] == 2 or nums[1] == 2:
+        return True
+    if nums[0] == 3 or nums[1] == 3:
+        return True
+    return False

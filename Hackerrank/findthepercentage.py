f906620c1c009a21a53f3fb660b89e9c3e8d74a5
Ben Math <benjamin.aaron.math@gmail.com>
2021-12-16 11:12:34 -0500

    hackerrank algorithms added to four stars

---
+++ Hackerrank/findthepercentage.py
@@ -0,0 +1,47 @@
+"""
+Input Format
+
+The first line contains the integer , the number of students' records. The next  lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
+
+Output Format
+
+Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+
+Sample Input 0
+
+3
+Krishna 67 68 69
+Arjun 70 98 63
+Malika 52 56 60
+Malika
+Sample Output 0
+
+56.00
+Explanation 0
+
+Marks for Malika are  whose average is 
+
+Sample Input 1
+
+2
+Harsh 25 26.5 28
+Anurag 26 28 30
+Harsh
+Sample Output 1
+
+26.50"""
+
+if __name__ == "__main__":
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        line = input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = input()
+    sum = 0
+
+    for score in student_marks[query_name]:
+        sum += score
+    print("{:.2f}".format(sum / len(student_marks[query_name])))
